3f2d44868c64d560bd5d3b071f2b53087e7ed5d4
etb <Hugo432121@gmail.com>
2018-09-26 00:08:40 +0800

    new idea

--- main.js
+++ main.js
@@ -10,3 +10,26 @@ $(window).on('scroll', function() {
     //set the background-color value
     header.css('background-color',`rgba(0,0,0,${progress*2 > 0.85 ? 0.85:progress*2})`);
 });
+
+var lat, lng;
+
+navigator.geolocation.getCurrentPosition(function(position) {
+    // console.log(position.coords.latitude, position.coords.longitude);
+    lat = position.coords.latitude, lng = position.coords.longitude;
+});
+
+// $.ajax({
+//     url: 'resource/data.json',
+//     type: 'GET',
+//     dataType: 'json',
+//     success: (msg) => {
+//         alert(msg);
+//     },
+//     error: (a,b,c) => {
+
+//     }
+// });
+
+$.get("resource/data.json",(data) => {
+    console.log(data);
+})
